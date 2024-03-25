a06f384baede9100e8e078fe526138b03146a0f8
bz2006 <bezelelb260@gmail.com>
2024-03-25 10:42:59 +0530

    D 25/3

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ import Checkout from "./pages/user/checkout"
 import OrderDetails from "./pages/Admin/orderDetails";
 import UserOrderDetails from "./pages/user/userorderDetails";
 
-axios.defaults.baseURL = "http://13.201.19.153"
+axios.defaults.baseURL = "http://43.205.119.18"
 axios.defaults.withCredentials = true
 
 
